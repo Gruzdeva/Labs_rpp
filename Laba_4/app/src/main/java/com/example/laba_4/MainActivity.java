0a37164d891d51d37fe770d0246f6007b7b2c430
Lera Gruzdeva <lera16042000@mail.ru>
2020-05-22 13:46:28 +0300

    new labs

--- Laba_4/app/src/main/java/com/example/laba_4/MainActivity.java
+++ Laba_4/app/src/main/java/com/example/laba_4/MainActivity.java
@@ -17,12 +17,10 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 
 public class MainActivity extends AppCompatActivity {
-    InfoRepository repository;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        repository = InfoRepository.createInstance(getApplicationContext());
         Button button = findViewById(R.id.btn);
         final DatePicker picker = findViewById(R.id.datePicker);
         button.setOnClickListener(new View.OnClickListener() {
@@ -31,15 +29,13 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Calendar calendar = new GregorianCalendar(picker.getYear(), picker.getMonth(), picker.getDayOfMonth());
                 Date date = calendar.getTime();
-                repository.insertDate(date);
-                stopService(new Intent(MainActivity.this, InfoService.class));
-                startForegroundService(new Intent(MainActivity.this, InfoService.class));
-                Intent intent = new Intent(MainActivity.this, MyWidget.class);
-                intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
-                int[] ids = AppWidgetManager.getInstance(getApplication())
-                        .getAppWidgetIds(new ComponentName(getApplication(), MyWidget.class));
-                intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, ids);
-                sendBroadcast(intent);
+
+
+
+
+
+
+
             }
         });
     }
